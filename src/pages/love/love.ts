c2ca99ae2329f545559dcc17ac3a445dc32e3c31
Vadim Rubinov <vazel978@msn.com>
2018-08-21 00:52:14 +0300

    ionic test

---
+++ src/pages/love/love.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+    selector: 'page-love',
+    templateUrl: 'love.html'
+})
+export class LovePage {
+
+    constructor(public navCtrl: NavController) {
+
+    }
+
+}
